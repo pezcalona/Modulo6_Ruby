d275144e834876f025ec3f753a072b7d73e55b9a
Pablo Escalona Osorio <pa.escalona.o@gmail.com>
2021-08-30 23:02:26 -0400

    agregando un espacio de prueba

---
+++ ejercicio_consola.rb
@@ -0,0 +1,16 @@
+puts "¡Hola Mundo!"
+puts("¿Cómo te llamas?")
+
+nombre_usuario = gets
+
+puts "hola normal " + nombre_usuario
+puts "hola compuesto #{nombre_usuario}"
+
+puts("¿Qué edad tienes?")
+
+edad = gets
+puts "#{edad} eso es mucho tiempo 💣"
+
+puts("¿dónde vives?")
+lugar_vivienda = gets
+puts "¿donde queda #{lugar_vivienda}?"
