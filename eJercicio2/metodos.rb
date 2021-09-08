590524222b4c09e5cd7654e817c0f0812ab19064
Pablo Escalona Osorio <pa.escalona.o@gmail.com>
2021-09-07 23:08:15 -0300

    modificiacion de carpetas

--- eJercicio2/metodos.rb
+++
@@ -1,7 +0,0 @@
-def dividir_entre_digitos(numeros)
-    resultado = numeros.each do |n|
-        n / n.to_slength
-    end
-end
-
-print dividir_entre_digitos([5,2,3,6,4,9,7])
---
+++ ejercicio3/metodos.rb
@@ -0,0 +1,9 @@
+# def dividir_entre_digitos(numeros)
+#     resultado = numeros.each do |n|
+#         n / n.to_slength
+#     end
+# end
+
+# print dividir_entre_digitos([5,2,3,6,4,9,7])
+
+Definir un método que pueda generar un arreglo de 10 posiciones con números de forma aleatoria entre el 0..100
